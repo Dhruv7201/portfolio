a2de06c93cdd36124319f932d3d25acf48aed95a
Dhruv Modi <d.modi@remoratech.io>
2025-04-25 19:34:16 +0530

    firebase + github deploy

--- src/components/qualifications/Data.jsx
+++ src/components/qualifications/Data.jsx
@@ -1,25 +1,25 @@
 export const experienceData = [
-    {
-      title: "Associate Software Engineer",
-      subtitle: "Ethics InfoTech",
-      calendar: "Aug 2023 - Jan 2024",
-    },
-    {
-      title: "Trainee Software Engineer",
-      subtitle: "Ethics InfoTech",
-      calendar: "Mar 2023 - Aug 2023",
-    },
+  {
+    title: "Associate Software Engineer",
+    subtitle: "Ethics InfoTech",
+    calendar: "Aug 2023 - Jan 2024",
+  },
+  {
+    title: "Trainee Software Engineer",
+    subtitle: "Ethics InfoTech",
+    calendar: "Mar 2023 - Aug 2023",
+  },
   ];
 
 export const educationData = [
-    {
-      title: "Bachelor's Degree",
-      subtitle: "Parul University",
-      calendar: "2020 - 2023",
-    },
-    {
-      title: "Diploma Computer Engineering",
-      subtitle: "Parul Technology",
-      calendar: "2016 - 2019",
-    },
-    ];
+  {
+    title: "Bachelor's Degree",
+    subtitle: "Parul University",
+    calendar: "2020 - 2023",
+  },
+  {
+    title: "Diploma Computer Engineering",
+    subtitle: "Parul Technology",
+    calendar: "2016 - 2019",
+  },
+];
