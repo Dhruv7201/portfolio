0079697d2462c29fe0bda5ac89348ea091d022db
Dhruv Modi <d.modi@remoratech.io>
2025-09-10 15:14:29 -0400

    changed Experience and projects

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -6,12 +6,12 @@ const Info = () => {
         <div className="about__box">
             <i className="bx bx-award"></i>
             <h3 className="about__title">Experience</h3>
-            <span className="about__subtitle">2 years</span>
+            <span className="about__subtitle">4.5 years</span>
         </div>
         <div className="about__box">
             <i className="bx bx-briefcase"></i>
             <h3 className="about__title">Completed</h3>
-            <span className="about__subtitle">Projects 12+</span>
+            <span className="about__subtitle">Projects 16+</span>
         </div>
     </div>
   )
